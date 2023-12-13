d108d7bb19c38dc84a11e6db6c8cce98f98ea585
PratikshaAM <115203730+PratikshaAM@users.noreply.github.com>
2023-12-13 15:55:08 +0000

    js

---
+++ 01_Basics/lec_9_summaryDataTypes.js
@@ -0,0 +1,45 @@
+//kis tarah data memory m rakha jata  h or access kia jata h 
+//usey 2 data types hote h
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //false
+
+// const bigNumber = 3456543576654356754n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"]; //reference type non primitive 
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+ 
+//Undefined => Undefined
+//Null =>obj
+//Boolean =>Boolean
+//Number =>Number
+//String=>String
